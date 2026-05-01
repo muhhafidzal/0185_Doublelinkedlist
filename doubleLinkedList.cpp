fd373aff32fd05fd8baeed6bbb013a38bcde12c9
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-05-01 16:57:16 +0700

    menambahkan menu utama program

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -212,3 +212,26 @@ public:
         }
     }
 };
+
+int main()
+{
+    DoubleLinkedList list;
+    char choice;
+
+    do
+    {
+
+        cout << "\nMenu\n";
+        cout << "1. Add Record\n";
+        cout << "2. Delete Record\n";
+        cout << "3. View ascending\n";
+        cout << "4. View descending\n";
+        cout << "5. Search record\n";
+        cout << "6. Exit\n";
+        cout << "Enter your choice: ";
+
+        cin >> choice;
+
+        
+    }
+}

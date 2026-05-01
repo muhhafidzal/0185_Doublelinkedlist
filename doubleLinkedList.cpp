591cabd37740e37e0ef92fb9a2909812406ea0f6
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-05-01 16:32:13 +0700

    fitur: menambahkan fungsi addNode

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -21,3 +21,10 @@ public:
     {
         START = NULL;
     }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+    }

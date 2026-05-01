27e19f5997cffe124548a379949bba80be8132bf
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-05-01 16:49:28 +0700

    implementasi traverse untuk output data

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -135,5 +135,21 @@ public:
             cout << "\nList is empty" << endl;
             return;
         }
+        
+        // Step 1: Mark first node as currentNode
+        Node *currentNode = START;
+
+        //Step 2: Repeat until currentNode == NULL
+        cout << "\nRecords in ascending order of roll number are:\n";
+        int i = 0;
+        while (currentNode != NULL)
+        {
+            
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+
+            // Step 3: Move to next node
+            currentNode = currentNode->next;
+            i++;
+        } 
     }
 }

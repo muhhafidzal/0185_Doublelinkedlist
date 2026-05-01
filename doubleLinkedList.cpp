85e718d7fc0e8e59294dcf767a1e80efc62d0da7
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-05-01 16:33:53 +0700

    fitur: alokasi node baru dan inisialisasi data

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -27,4 +27,11 @@ public:
         int nim;
         cout << "\nEnter the roll number of the student: ";
         cin >> nim;
+       
+        // Step 1: Allocate memory for new node
+        Node *newNode = new Node();
+
+        // Step 2: Assign value to the data field
+        newNode->noMhs = nim;
+
     }
